bd1a6d604c7de8614826a7d0a0c9047a5d4eeee2
Gregory Williams <gregwills85@gmail.com>
2025-04-06 11:42:46 -0600

    uart-hdl: test: Remove duplicate code from baud_gen
    Found a way to put duplicate processes in a generate statement removing
    most of the duplicate code.
    
    Signed-off-by: Gregory Williams <gregwills85@gmail.com>

--- uart-hdl/tests/baud_gen_tb.vhd
+++ uart-hdl/tests/baud_gen_tb.vhd
@@ -12,20 +12,21 @@ architecture Behavioral of baud_gen_tb is
 	signal d_in, d_out: std_logic_vector(7 downto 0) := (others => '0');
 
 	--signal arrays
-	type slv_int_array is array (0 to 2) of std_logic_vector(15 downto 0);
-	type slv_frac_array is array (0 to 2) of std_logic_vector(5 downto 0);
+	type slv_int_array is array (0 to 3) of std_logic_vector(15 downto 0);
+	type slv_frac_array is array (0 to 3) of std_logic_vector(5 downto 0);
 	signal int_div: slv_int_array := (others => (others => '0'));
 	signal frac_div: slv_frac_array := (others => (others => '0'));
-	signal s_tick, finished: std_logic_vector(2 downto 0) := (others => '0');
+	signal s_tick, finished: std_logic_vector(3 downto 0) := (others => '0');
 
 	--baud rate calculations
-	type int_array is array (0 to 2) of integer;
+	type int_array is array (0 to 3) of integer;
 	signal tick_count, baud_count: int_array := (others => 0);
+	signal baud_desired: int_array := (230400, 115200, 38400, 9600);
 
 	constant sec_divider: integer := 20;
 begin
 
-	gen_baud_tests: for i in 0 to 2 generate
+	gen_baud_tests: for i in 0 to 3 generate
 		baud_gen_uut_0: entity work.baud_generator
 		Port map(
 			clk => clk,
@@ -34,95 +35,60 @@ begin
 			frac_div => frac_div(i),
 			s_tick => s_tick(i)
 		);
+
+		process
+			variable start_time: time := 0 sec;
+		begin
+
+			start_time := now;
+			while now < start_time + (1 sec / sec_divider) loop
+				wait until rising_edge(s_tick(i));
+				tick_count(i) <= tick_count(i) + 1;
+				if tick_count(i) = 15 then
+					baud_count(i) <= baud_count(i) + 1;
+					tick_count(i) <= 0;
+				end if;
+			end loop;
+
+			report "Baud " & integer'image(baud_desired(i)) &
+				" projected baud ticks: " & integer'image(baud_count(i) * sec_divider);
+			finished(i) <= '1';
+			wait;
+		end process;
 	end generate gen_baud_tests;
 
 	rst <= '0';
 
 	--clk
-	clk <= not clk after clk_period/2 when finished /= "111" else '0';
+	clk <= not clk after clk_period/2 when finished /= "1111" else '0';
 
-	--baud_generator 0
-	process
-		variable start_time: time := 0 sec;
-		constant baud_desired: integer := 115200;
-	begin
-		--115200 baud
-		--125 Mhz clock (125 x 10^6) / (16 * 115200) = 67.81684
-		--integer divisor = 67
-		--fraction divisor = floor(0.81684 * 64 + 0.5) = 52
-		int_div(0) <= std_logic_vector(to_unsigned(67, 16));
-		frac_div(0) <= std_logic_vector(to_unsigned(52, 6));
+	--230400 baud
+	--125 Mhz clock (125 x 10^6) / (16 * 230400) = 33.90842
+	--integer divisor = 33
+	--fraction divisor = floor(0.90842 * 64 + 0.5) = 58
+	int_div(0) <= std_logic_vector(to_unsigned(33, 16));
+	frac_div(0) <= std_logic_vector(to_unsigned(58, 6));
 
-		start_time := now;
-		while now < start_time + (1 sec / sec_divider) loop
-			wait until rising_edge(s_tick(0));
-			tick_count(0) <= tick_count(0) + 1;
-			if tick_count(0) = 15 then
-				baud_count(0) <= baud_count(0) + 1;
-				tick_count(0) <= (0);
-			end if;
-		end loop;
+	--115200 baud
+	--125 Mhz clock (125 x 10^6) / (16 * 115200) = 67.81684
+	--integer divisor = 67
+	--fraction divisor = floor(0.81684 * 64 + 0.5) = 52
+	int_div(1) <= std_logic_vector(to_unsigned(67, 16));
+	frac_div(1) <= std_logic_vector(to_unsigned(52, 6));
 
-		report "Baud " & integer'image(baud_desired) & " projected baud ticks: " & integer'image(baud_count(0) * sec_divider);
+	--38400 baud
+	--125 Mhz clock (125 x 10^6) / (16 * 38400) = 203.4505
+	--integer divisor = 203
+	--fraction divisor = floor(0.4505 * 64 + 0.5) = 29
+	int_div(2) <= std_logic_vector(to_unsigned(203, 16));
+	frac_div(2) <= std_logic_vector(to_unsigned(29, 6));
 
-		finished(0) <= '1';
-		wait;
-	end process;
+	--9600 baud
+	--125 Mhz clock (125 x 10^6) / (16 * 9600) = 813.80
+	--integer divisor = 813
+	--fraction divisor = floor(0.802 * 64 + 0.5) = 51
+	int_div(3) <= std_logic_vector(to_unsigned(813, 16));
+	frac_div(3) <= std_logic_vector(to_unsigned(51, 6));
 
-	--baud_generator 1
-	process
-		variable start_time: time := 0 sec;
-		constant baud_desired: integer := 38400;
-	begin
-		--38400 baud
-		--125 Mhz clock (125 x 10^6) / (16 * 38400) = 203.4505
-		--integer divisor = 203
-		--fraction divisor = floor(0.4505 * 64 + 0.5) = 29
-		int_div(1) <= std_logic_vector(to_unsigned(203, 16));
-		frac_div(1) <= std_logic_vector(to_unsigned(29, 6));
-
-		start_time := now;
-		while now < start_time + (1 sec / sec_divider) loop
-			wait until rising_edge(s_tick(1));
-			tick_count(1) <= tick_count(1) + 1;
-			if tick_count(1) = 15 then
-				baud_count(1) <= baud_count(1) + (1);
-				tick_count(1) <= 0;
-			end if;
-		end loop;
-
-		report "Baud " & integer'image(baud_desired) & " projected baud ticks: " & integer'image(baud_count(1) * sec_divider);
-
-		finished(1) <= '1';
-		wait;
-	end process;
-
-	--baud_generator 2
-	process
-		variable start_time: time := 0 sec;
-		constant baud_desired: integer := 9600;
-	begin
-		--9600 baud
-		--125 Mhz clock (125 x 10^6) / (16 * 9600) = 813.80
-		--integer divisor = 813
-		--fraction divisor = floor(0.802 * 64 + 0.5) = 51
-		int_div(2) <= std_logic_vector(to_unsigned(813, 16));
-		frac_div(2) <= std_logic_vector(to_unsigned(51, 6));
-
-		start_time := now;
-		while now < start_time + (1 sec / sec_divider) loop
-			wait until rising_edge(s_tick(2));
-			tick_count(2) <= tick_count(2) + 1;
-			if tick_count(2) = 15 then
-				baud_count(2) <= baud_count(2) + 1;
-				tick_count(2) <= 0;
-			end if;
-		end loop;
-
-		report "Baud " & integer'image(baud_desired) & " projected baud ticks: " & integer'image(baud_count(2) * sec_divider);
-
-		finished(2) <= '1';
-		wait;
-	end process;
 
 end Behavioral;

d472666c32feded09b5b2ef6f24df30153898747
Gregory Williams <gregwills85@gmail.com>
2025-05-06 11:55:21 -0600

    uart-hdl: tests: Finish tests for axil module
    Finished test bench for the axil control module. Tests control outputs
    as well as axi read backs when writing to registers
    
    Signed-off-by: Gregory Williams <gregwills85@gmail.com>

--- uart-hdl/tests/axil_control_tb.vhd
+++ uart-hdl/tests/axil_control_tb.vhd
@@ -37,29 +37,33 @@ architecture Behavioral of axil_control_tb is
 	signal tx_fifo_wr: std_logic := '0';
 
 	--flag register
+	signal UARTFR: std_logic_vector(4 downto 0) := b"00100"; --address
 	signal fifo_full, fifo_near_full, fifo_empty: std_logic := '0';
 	signal tx_busy, tx_cts: std_logic := '0';
 
 	--baudrate
+	signal UARTIBRD: std_logic_vector(4 downto 0) := b"01000"; --address
 	signal baud_int_div: std_logic_vector(15 downto 0) := (others => '0');
+	signal UARTFBRD: std_logic_vector(4 downto 0) := b"01100"; --address
 	signal baud_frac_div: std_logic_vector(5 downto 0) := (others => '0');
 
 	--line control
+	signal UARTLCR: std_logic_vector(4 downto 0) := b"10000"; --address
 	signal break_gen, stop_bits: std_logic := '0';
 	signal parity_config, data_bits: std_logic_vector(1 downto 0) := (others => '0');
 
 	--control
+	signal UARTCTRL: std_logic_vector(4 downto 0) := b"10100"; --address
 	signal flow_ctrl_enable, rts: std_logic := '0';
 	signal uart_enable, rx_enable, tx_enable: std_logic := '0';
 
 	--fifo signals
 	signal fifo_dout: std_logic_vector(7 downto 0) := (others => '0');
 
-	type addr_array is array (0 to 3) of std_logic_vector(4 downto 0);
-	type data_array is array (0 to 3) of std_logic_vector(31 downto 0);
+	type data_array is array (0 to 7) of std_logic_vector(31 downto 0);
 
 	procedure write_axi (
-			addr: in addr_array;
+			addr: in std_logic_vector(4 downto 0);
 			data: in data_array;
 			num_txns: in integer;
 			axi_error_flag: out boolean;
@@ -84,7 +88,7 @@ architecture Behavioral of axil_control_tb is
 		axil_wvalid  <= '1';
 		for i in 0 to num_txns-1 loop
 			--write transaction
-			axil_awaddr  <= addr(i);
+			axil_awaddr  <= addr;
 			axil_wdata   <= data(i);
 			axil_wstrb   <= "1111";
 			axil_bready  <= '1';
@@ -107,7 +111,7 @@ architecture Behavioral of axil_control_tb is
 	end write_axi;
 
 	procedure read_axi (
-			addr: in addr_array;
+			addr: in std_logic_vector(4 downto 0);
 			data: out data_array;
 			num_txns: in integer;
 			axi_error_flag: out boolean;
@@ -126,7 +130,7 @@ architecture Behavioral of axil_control_tb is
 		axi_error_flag := false;
 
 		for i in 0 to num_txns-1 loop
-			axil_araddr  <= addr(i);
+			axil_araddr  <= addr;
 			axil_arvalid <= '1';
 			axil_rready  <= '1';
 			wait until rising_edge(axil_clk) AND axil_rvalid = '1';
@@ -222,24 +226,128 @@ begin
 	--rx data needs 12 bits just prepend for test
 	rx_fifo_data <= x"0" & fifo_dout;
 	process
-		variable test_addrs: addr_array := (UARTDR, UARTDR, others => (others => '0'));
-		variable test_data: data_array := (x"DEADBEEF", x"BEEFDEAD", others => (others => '0'));
-		variable ret_data: data_array := (others => (others => '0'));
+		variable mask: std_logic_vector(31 downto 0) := (others => '0');
+		variable test_data: data_array := (others => (others => '0'));
+		variable ret_data:  data_array := (others => (others => '0'));
 		variable axi_error: boolean := false;
 	begin
 		wait until rising_edge(clk) AND rst = '1';
 		wait for clk_period;
 
-		write_axi(test_addrs, test_data, 2, axi_error, clk, awaddr, awvalid, awready,
+		-- check that empty flags are on
+		read_axi(UARTFR, ret_data, 1, axi_error, clk, araddr, arvalid, arready,
+			 rdata, rresp, rvalid, rready);
+		if ret_data(0)(5 downto 0) /= "101000" then
+			report "TEST_ERROR: axi does not report fifo empty on start";
+		end if;
+		ret_data := (others => (others => '0'));
+
+		--fill up fifo
+		test_data := (x"DEADBEEF", x"BEEFDEAD", x"12345678", x"87654321",
+			      x"10ABCDEF", x"FFFFFFFF", x"55555555", x"1A2B3C4D");
+		write_axi(UARTDR, test_data, 8, axi_error, clk, awaddr, awvalid, awready,
 			  wdata, wvalid, wready, wstrb, bresp, bvalid, bready);
 
-		read_axi(test_addrs, ret_data, 2, axi_error, clk, araddr, arvalid, arready,
+		--check fifo full
+		read_axi(UARTFR, ret_data, 1, axi_error, clk, araddr, arvalid, arready,
 			 rdata, rresp, rvalid, rready);
-		for i in 0 to 1 loop
+		if ret_data(0)(5 downto 0) /= "010100" then
+			report "TEST_ERROR: axi does not report fifo empty on start";
+		end if;
+
+		-- readback data from fifo
+		read_axi(UARTDR, ret_data, 8, axi_error, clk, araddr, arvalid, arready,
+			 rdata, rresp, rvalid, rready);
+		for i in 0 to 7 loop
 			if ret_data(i)(7 downto 0) /= test_data(i)(7 downto 0) then
 				report "TEST_ERROR: data register mismatch";
 			end if;
 		end loop;
+
+		-- check that empty flags are on
+		read_axi(UARTFR, ret_data, 1, axi_error, clk, araddr, arvalid, arready,
+			 rdata, rresp, rvalid, rready);
+		if ret_data(0)(5 downto 0) /= "101000" then
+			report "TEST_ERROR: axi does not report fifo empty after reads";
+		end if;
+
+		--write to baud gen registers
+		test_data := (x"DEADBEEF", others => (others => '0'));
+		write_axi(UARTIBRD, test_data, 1, axi_error, clk, awaddr, awvalid, awready,
+			  wdata, wvalid, wready, wstrb, bresp, bvalid, bready);
+		-- check control out
+		if baud_int_div /= test_data(0)(15 downto 0) then
+			report "TEST_ERROR: integer baud divisor output not correct";
+		end if;
+
+		-- check axi readback
+		read_axi(UARTIBRD, ret_data, 1, axi_error, clk, araddr, arvalid, arready,
+			 rdata, rresp, rvalid, rready);
+		mask := (31 downto 16 => '0') & (15 downto 0 => '1');
+		if ret_data(0) /= (test_data(0) AND mask) then
+			report "TEST_ERROR: integer baud divisor register readback not correct";
+		end if;
+
+		write_axi(UARTFBRD, test_data, 1, axi_error, clk, awaddr, awvalid, awready,
+			  wdata, wvalid, wready, wstrb, bresp, bvalid, bready);
+		-- check control out
+		if baud_frac_div /= test_data(0)(5 downto 0) then
+			report "TEST_ERROR: fractional baud divisor output not correct";
+		end if;
+
+		-- check axi readback
+		read_axi(UARTFBRD, ret_data, 1, axi_error, clk, araddr, arvalid, arready,
+			 rdata, rresp, rvalid, rready);
+		mask := (31 downto 6 => '0') & (5 downto 0 => '1');
+		if ret_data(0) /= (test_data(0) AND mask) then
+			report "TEST_ERROR: fractional baud divisor register readback not correct";
+		end if;
+
+		-- write to line control
+		test_data := (x"DEADBEEF", others => (others => '0'));
+		write_axi(UARTLCR, test_data, 1, axi_error, clk, awaddr, awvalid, awready,
+			  wdata, wvalid, wready, wstrb, bresp, bvalid, bready);
+		if data_bits /= test_data(0)(5 downto 4) then
+			report "TEST_ERROR: data bits control output not correct";
+		elsif stop_bits /= test_data(0)(3) then
+			report "TEST_ERROR: stop bits control output not correct";
+		elsif parity_config /= test_data(0)(2 downto 1) then
+			report "TEST_ERROR: parity config control output not correct";
+		elsif break_gen /= test_data(0)(0) then
+			report "TEST_ERROR: break gen control output not correct";
+		end if;
+
+		-- check axi readback
+		read_axi(UARTLCR, ret_data, 1, axi_error, clk, araddr, arvalid, arready,
+			 rdata, rresp, rvalid, rready);
+		mask := (31 downto 6 => '0') & (5 downto 0 => '1');
+		if ret_data(0) /= (test_data(0) AND mask) then
+			report "TEST_ERROR: line control register readback not correct";
+		end if;
+
+		-- write to control
+		test_data := (x"DEADBEEF", others => (others => '0'));
+		write_axi(UARTCTRL, test_data, 1, axi_error, clk, awaddr, awvalid, awready,
+			  wdata, wvalid, wready, wstrb, bresp, bvalid, bready);
+		if flow_ctrl_enable /= test_data(0)(4) then
+			report "TEST_ERROR: flow control enable output not correct";
+		elsif rts /= test_data(0)(3) then
+			report "TEST_ERROR: rts control output not correct";
+		elsif rx_enable /= test_data(0)(2) then
+			report "TEST_ERROR: rx_enable control output not correct";
+		elsif tx_enable /= test_data(0)(1) then
+			report "TEST_ERROR: tx_enable control output not correct";
+		elsif uart_enable /= test_data(0)(0) then
+			report "TEST_ERROR: uart_enable control output not correct";
+		end if;
+
+		-- check axi readback
+		read_axi(UARTCTRL, ret_data, 1, axi_error, clk, araddr, arvalid, arready,
+			 rdata, rresp, rvalid, rready);
+		mask := (31 downto 5 => '0') & (4 downto 0 => '1');
+		if ret_data(0) /= (test_data(0) AND mask) then
+			report "TEST_ERROR: line control register readback not correct";
+		end if;
 
 		report "TEST_SUCCESS: end of test";
 		finished <= '1';
